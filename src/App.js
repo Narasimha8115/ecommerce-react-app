2b2c020dd18d2ecfe149d819e523abbfabc56b97
chavithina lakshmi narasimha <pandulaxmi44@gmail.com>
2022-11-14 11:19:44 +0530

    header component builded with options of shop and conatact and logo

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Route,Switch } from "react-router-dom";
 
 import ShopPage from './pages/shop/shop.component';
 
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={ Homepage }/>
         <Route path='/shop' component={ ShopPage } />
