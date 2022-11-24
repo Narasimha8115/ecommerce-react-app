c593c947220644cc12c8a0ee2a4e6e3db3daf3f8
chavithina lakshmi narasimha <pandulaxmi44@gmail.com>
2022-11-24 17:04:10 +0530

    signin handlesubmit ipdated

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss';
 
-import { signInWithGoogle } from '../../firebase/firebase-utils';
+import { auth,signInWithGoogle } from '../../firebase/firebase-utils';
 import { Link } from 'react-router-dom';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -13,8 +13,15 @@ class SignIn extends React.Component {
             password:''
          }
     }
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
+        const {email,password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email,password)
+            this.setState({email:'',password:''});
+        }catch (error){
+            console.log(error);
+        }
         this.setState({email:'',password:''})
     }
     handleChange = event =>{
