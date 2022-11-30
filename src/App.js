d1113c99eae92d03b95fa6767ff7466138fe3620
chavithina lakshmi narasimha <pandulaxmi44@gmail.com>
2022-11-30 10:43:55 +0530

    updated our app to redirect to homepage when the user signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-import { Route,Switch } from "react-router-dom";
+import { Route,Switch,Redirect } from "react-router-dom";
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignup from './pages/signin-and-signup/signin-and-signup.component';
@@ -43,20 +43,32 @@ class App extends React.Component{
     return (
       <div>
         <Header />
+        
         <Switch>
-          <Route exact path='/' component={ Homepage }/>
+          <Route  exact path='/' component={ Homepage }/>
+
           <Route path='/shop' component={ ShopPage } />
-          <Route path="/signin" component={SignInAndSignup} />
-          <Route path="/signup" component={ SignupSingle} />
+
+          <Route  exact path='/signin' render = {() => this.props.currentUser ?
+          (<Redirect to = '/' />):(<SignInAndSignup />)
+          } />
+
+          <Route   path='/signup' component={ SignupSingle} />
+
         </Switch>
+
       </div>
     );
 
   }
 }
+
+const mapStateToProps = ({ user })=> ({
+  currentUser:user.currentUser
+});
 const mapDispatchToProps = dispatch => ({
 
   setCurrentUser:user => dispatch(setCurrentUser(user))
 
 });
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
